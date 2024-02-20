8e3e1137dbaf4ddd9927b18bde82f94198cd0e9a
Fehings <frances.hutchings@ncl.ac.uk>
2024-02-20 23:44:23 +0000

    created catalog app and aded pointer to it in settings

--- claytontv/settings.py
+++ claytontv/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog.apps.CatalogConfig', # video catalogue, lives in /catalog/apps.py
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
