84d9b771f93551f1520033dca08c2d9a371aa237
Fehings <frances.hutchings@ncl.ac.uk>
2024-02-22 13:03:11 +0000

    added to urls as per tutorial and used migrate to create and update db

--- claytontv/urls.py
+++ claytontv/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include #used to add paths from other apps
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static # using to serve static files to havea dummy dev page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='catalog/')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
