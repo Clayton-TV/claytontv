ff18263353d49a20299f7690d1deb598a0f10629
Jamie Gardner <jamie@tgo.dev>
2024-04-17 19:11:36 +0100

    Create sample views for catalogue and livestreams

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Catalogue.")
+
+
+def show(request, video_id):
+    return HttpResponse(f"Showing video {video_id}.")
