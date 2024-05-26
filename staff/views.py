4036cd5a2167abc87490ab60cdd6dddb161f6d70
Jonathan Sunarja <jonathansunarja@yahoo.com>
2024-05-26 17:58:20 +0100

    WIP /staff/videos index page

--- staff/views.py
+++ staff/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render
-import django
 from inertia import render
 
 
 def index(request):
-    django_version = django.get_version()
-
-    return render(request, 'Staff.Videos', {
-        'djangoVersion': django_version,
-    })
+    return render(request, 'Staff/Index')
 
 def create(request):
     return render(request, 'Staff/Create')
