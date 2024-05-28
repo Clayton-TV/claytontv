052e162e2db89af5ad585f57e769b5a778b40584
Fehings <frances.hutchings@ncl.ac.uk>
2024-05-28 18:34:35 +0100

    updated the video table model file to match DB design

--- catalogue/models/video.py
+++ catalogue/models/video.py
@@ -1,20 +1,37 @@
-from django.db import models
+from django.db import models    # import the model class that all models are based on
 from django.urls import reverse # generate urls by reversing url pattern
 
-class Video(models.Model):
-    """Model representing a video"""
-    title = models.CharField(max_length=200)
-    channel = models.ForeignKey('Channel',on_delete=models.RESTRICT, null=True)
+# Now import the models that we need to link to:
+from .channel import Channel    
+from .topic import Topic
+from .series import Series
+from .speaker import Speaker
+from .bible_book import Bible_book
+from .ministry import Ministry
+from .demograpic import Demographic
+from .label import Label
+
+# Now the actual class definition for the model (database table):
+class Video(models.Model): 
+    """Model representing the database table for videos,
+    where each table entry is an individual video"""
+    title = models.CharField(max_length=200) # check max title length on popular upload sites
     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the video")
+    url = models.URLField(unique=True)
+    id_number = models.CharField(max_length=100,unique=True)
+    date_created = models.DateField()
+    date_modified = models.DateField(null=True, blank=True)
+    date_recorded = models.DateField(null=True, blank=True)
+    is_livestream = models.BooleanField(default=False)
+    channel = models.ForeignKey('Channel',on_delete=models.RESTRICT, null=True)
     topic = models.ManyToManyField(Topic, help_text="Select topics for this video")
     series = models.ForeignKey('Series', on_delete=models.RESTRICT, null=True, blank=True)
-    speaker = models.ForeignKey('Speaker',on_delete=models.RESTRICT, null=True, blank=True)
     series_number = models.IntegerField(help_text="If part of a series provide the number in the series", null=True, blank=True)
-    date_created = models.DateField()
-    livestream_date = models.DateField(null=True, blank=True)
-    embedding_url = models.URLField(unique=True)
-    bible_book = models.CharField(max_length=100, null=True, blank=True)
+    speaker = models.ManyToManyField('Speaker',on_delete=models.RESTRICT, null=True, blank=True)
     ministry = models.ManyToManyField('Ministry', null=True, blank=True)
+    bible_book = models.ManyToManyField('Bible_book', on_delete=models.RESTRICT, null=True, blank=True)
+    label = models.ForeignKey('Label',null=True, blank=True)
+    demographic = models.ManyToManyField('Demographic',null=True,blank=True)
     
     class Meta:
         ordering = ['date_created']
