f94b8a67edeb6d944120e07e7fe0842b41000c2c
Fehings <frances.hutchings@ncl.ac.uk>
2024-05-30 22:36:11 +0100

    added models for the label table

--- catalogue/models/label.py
+++ catalogue/models/label.py
@@ -1,2 +1,16 @@
 from django.db import models
 from django.urls import reverse # generate urls by reversing url pattern
+
+class Label(models.Model):
+    """Model representing a table for the admin labels to denote the video type and storage"""
+    label_series = models.CharField(max_length=1000)
+    label_subseries = models.CharField(max_length=1000)
+    label_episode = models.CharField(max_length=1000)
+    
+    def __str__(self):
+        """String for representing model object"""
+        return self.name
+    
+    def get_absolute_url(self):
+        """Returns the URL to access a detailed record for the speaker"""
+        return reverse("speaker-detail", args=[str(self.id)])
