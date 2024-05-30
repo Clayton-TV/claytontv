33993c957c584a88fec2ba6b4cf1fd985f05227e
Fehings <frances.hutchings@ncl.ac.uk>
2024-05-30 23:32:29 +0100

    Added imports for new tables to models init script

--- catalogue/models/__init__.py
+++ catalogue/models/__init__.py
@@ -4,6 +4,8 @@ from .series import Series
 from .ministry import Ministry
 from .speaker import Speaker
 from .topic import Topic
+from .demograpic import Demographic
+from .label import Label
 
 # NB will need to import models from the other tables
 # when the classes for these have been created
