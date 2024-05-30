626a2597c1b8907c73635662144e75c946054316
Fehings <frances.hutchings@ncl.ac.uk>
2024-05-30 22:54:26 +0100

    Adding models for demographic table

--- catalogue/models/demograpic.py
+++ catalogue/models/demograpic.py
@@ -1,2 +1,19 @@
 from django.db import models
 from django.urls import reverse # generate urls by reversing url pattern
+
+class Demographic(models.Model):
+    """Model representing the demographic table, intended to allow easy segregation of the website for different user types."""
+    name = models.CharField(max_length=200) # e.g. kids, searchers
+    summary = models.TextField(max_length=1000)
+    series = models.ManyToManyField('Series',null=True, blank=True)
+    topics = models.ManyToManyField('Topic',null=True, blank=True)
+    videos = models.ManyToManyField('Video',null=True, blank=True)
+    thumbnail = models.CharField(max_length=200)
+    
+    def __str__(self):
+        """String for representing model object"""
+        return self.name
+    
+    def get_absolute_url(self):
+        """Returns the URL to access a detailed record for the speaker"""
+        return reverse("speaker-detail", args=[str(self.id)])
