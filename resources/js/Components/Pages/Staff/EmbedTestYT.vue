8742c92482d087847b970b35f4f235918bef74cf
JL1996fc <71602581+JL1996fc@users.noreply.github.com>
2024-06-23 16:34:17 +0100

    added vimeo test Link

--- resources/js/Components/Pages/Staff/EmbedTestYT.vue
+++ resources/js/Components/Pages/Staff/EmbedTestYT.vue
@@ -3,9 +3,14 @@ import { Head } from "@inertiajs/vue3"
 </script>
 
 <template>
+    <h1>YouTube</h1>
     <Head title="Staff - Videos - Embed Test (YouTube)" />
 
     <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/pD2kq9cSw2Y?si=TKKx8JFDw_LRF3SW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+    <h1>vimeo</h1>
+    <iframe src="https://player.vimeo.com/video/**yourvideonumberhere**" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+
 </template>
 
 <style scoped>
