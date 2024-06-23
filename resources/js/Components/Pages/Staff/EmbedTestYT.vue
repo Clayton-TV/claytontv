6183a9912ffbd9f183dc9776fa09529b728027eb
Matthew Bunting <mtbu@tutanota.com>
2024-06-23 16:11:06 +0100

    Add youtube embed test

---
+++ resources/js/Components/Pages/Staff/EmbedTestYT.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Head } from "@inertiajs/vue3"
+</script>
+
+<template>
+    <Head title="Staff - Videos - Embed Test (YouTube)" />
+
+    <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/pD2kq9cSw2Y?si=TKKx8JFDw_LRF3SW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</template>
+
+<style scoped>
+</style>
--- staff/urls.py
+++ staff/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('videos/create', views.create, name='create'), # /staff/videos/create
     path('videos/update', views.update, name='update'), # /staff/videos/update
     path('videos/delete', views.delete, name='delete'), # /staff/videos/delete
+    path('videos/embedtest-yt', views.embedtestyt, name='embedtestyt'), # /staff/videos/embedtestyt
     #path('<int:video_id>/', views.show, name='show'),
 ]
