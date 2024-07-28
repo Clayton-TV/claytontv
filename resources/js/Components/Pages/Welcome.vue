dac1e3451f572c68743f08ba2b0a079dfb6ac5f3
JL1996fc <71602581+JL1996fc@users.noreply.github.com>
2024-07-28 14:10:40 +0100

    added the video card in welecome

--- resources/js/Components/Pages/Welcome.vue
+++ resources/js/Components/Pages/Welcome.vue
@@ -229,6 +229,26 @@ const mobileMenuOpen = ref(false)
         </header>
 
         <main class="isolate">
+            <div class="flex justify-center">
+    <div class="rounded-lg shadow-lg bg-white max-w-sm">
+        <a href="#!">
+            <video width="320" height="240" controls class="w-full rounded-t-lg">
+                <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
+                Your browser does not support the video tag.
+            </video>
+        </a>
+        <div class="p-6">
+            <h5 class="text-gray-900 text-xl font-medium mb-2">Video Card</h5>
+            <p class="text-gray-700 text-base mb-4">
+                Some quick example text to build on the card title and make up the bulk of the card's
+                content.
+            </p>
+            <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+        </div>
+    </div>
+</div>
+        
             <!-- Hero section -->
             <div class="relative pt-14">
                 <div
