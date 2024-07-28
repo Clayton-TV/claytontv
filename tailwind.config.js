40e37aad2e123bf13eb8d53f287028bcf4454958
Jamie Gardner <jamie@tgo.dev>
2024-07-28 14:47:28 +0100

    Set the default gray to zinc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+import colors from "tailwindcss/colors.js"
+
+/** @type {import("tailwindcss").Config} */
 export default {
-  content: [
-      "./templates/*.html",
-      "./resources/js/**/*.{html,vue}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./templates/*.html",
+        "./resources/js/**/*.{html,vue}",
+    ],
+    theme: {
+        colors: {
+            gray: colors.zinc,
+        },
+        extend: {},
+    },
+    plugins: [],
 }
 
