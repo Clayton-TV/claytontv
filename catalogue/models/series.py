938dd1051d941c60bb6cef98b1a584642b3dbfb2
Missive00 <jonathan.jf.land@gmail.com>
2024-07-28 13:56:05 +0100

    All the help text
    All the help text
    
    Co-Authored-By: 萧沛 <menuet@qq.com>
    Co-Authored-By: Thomas Eichmann <41003+the@users.noreply.github.com>

--- catalogue/models/series.py
+++ catalogue/models/series.py
@@ -17,8 +17,8 @@ class Series(models.Model):
     ministry = models.ManyToManyField('Ministry', null=True, blank=True, related_name='+', help_text= 'The ministry related to the series.')
     videos = models.ManyToManyField('Video', null=True, blank=True, related_name='+', help_text= 'The videos related to the series.')
     bible_book = models.ManyToManyField(Bible_Book, null=True, blank=True, help_text= 'The Bible books related to the series.')
-    year_start = models.CharField(max_length=100, help_text= 'The ')
-    year_end = models.CharField(max_length=100)
+    year_start = models.CharField(max_length=100, help_text= 'The year the series started.')
+    year_end = models.CharField(max_length=100, help_text= 'The year the series ended.')
 
     def __str__(self):
         """String for representing model object"""
