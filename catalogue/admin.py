62775f05985630ee672c77a668dc3bd3ef36523b
Fehings <frances.hutchings@ncl.ac.uk>
2024-07-28 16:22:24 +0100

    updated admin.py to render bd info in default admin pages

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Video, Bible_Book, Channel, Demographic, Label, Ministry, Series, Speaker, Topic
+
+admin.site.register(Video)
+admin.site.register(Bible_Book)
+admin.site.register(Channel)
+admin.site.register(Demographic)
+admin.site.register(Label)
+admin.site.register(Ministry)
+admin.site.register(Series)
+admin.site.register(Speaker)
+admin.site.register(Topic)
