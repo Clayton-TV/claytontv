7494fbe64c165840e6f91cbe1231be3341a08f44
Missive00 <jonathan.jf.land@gmail.com>
2024-08-25 15:25:20 +0100

    still no work :(

--- app/Utilities/importer.py
+++ app/Utilities/importer.py
@@ -1,6 +1,4 @@
-import os
-import sys
-import django
+from django.db import models
 from django.urls import reverse  # generate urls by reversing url pattern
 import csv
 from catalogue.models.bible_book import Bible_Book
@@ -20,5 +18,4 @@ def import_bible(bible_path):
 
 if __name__ == '__main__':
     Bible_CSV_Path = 'CSV/Bible Books.csv'
-    django.setup()
     import_bible(Bible_CSV_Path)
--- app/settings.py
+++ app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'catalogue.apps.CatalogueConfig',
     'livestreams.apps.LivestreamsConfig',
     'django_seed',
+    'importer',
 ]
 
 MIDDLEWARE = [
