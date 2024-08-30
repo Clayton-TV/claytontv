c5fefa651abb76563e10b0f4a51c484f218076aa
Matthew Bunting <mtbu@tutanota.com>
2024-08-30 23:03:35 +0100

    Commit missing files...

---
+++ resources/js/Components/Layouts/Navigation.vue
@@ -0,0 +1,9 @@
+<template>
+    <header class="bg-white">
+        <nav class="mx-auto flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+            <h1 class="my-4 mx-auto text-5xl font-bold text-gray-800">
+                Clayton TV
+            </h1>
+        </nav>
+    </header>
+</template>
---
+++ resources/js/Components/Pages/Welcome.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Head, Link } from "@inertiajs/vue3"
+import VideoCardList from "@/Organisms/VideoCardList.vue"
+
+defineProps({
+    videos: {
+        type: Array,
+    },
+})
+
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <div class="main-container p-8">
+        <main class="isolate">
+            <VideoCardList :videos="videos" />
+        </main>
+    </div>
+</template>
