9a88b9148f7735538b1bcf2e697070db60e7d014
Matthew Bunting <mtbu@tutanota.com>
2024-09-13 21:58:49 +0100

    Only show first half of A-Z bar on smaller screens
    Shows three dots at right-hand side on smaller screens.
    Needs additional work to allow flipping between the halves
    by pressing the dots.

--- resources/js/Components/Organisms/AZBar.vue
+++ resources/js/Components/Organisms/AZBar.vue
@@ -3,11 +3,17 @@
         <div class="text-lg px-3 pb-2">
             Filter A-Z:
         </div>
-        <ul class="grid grid-flow-col auto-cols-fr text-nowrap text-center text-xs sm:text-sm md:text-lg lg:text-xl" role="radio">
-            <li v-for="x in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'" class="m-0.5 border-2 rounded border-gray-400 hover:border-red-600">
+        <ul class="grid grid-flow-col auto-cols-fr text-nowrap text-center text-lg" role="radio">
+            <li v-for="x in 'ABCDEFGHIJKLM'" class="m-0.5 border-2 rounded border-gray-400 hover:border-red-600">
                 {{x}}
             </li>
-            <li class="m-0.5 border-2 rounded border-gray-400 hover:border-red-600">
+            <li class="lg:hidden xl:hidden 2xl:hidden">
+            ...
+            </li>
+            <li v-for="x in 'NOPQRSTUVWXYZ'" class="m-0.5 border-2 rounded border-gray-400 hover:border-red-600 hidden lg:inline xl:inline 2xl:inline">
+                {{x}}
+            </li>
+            <li class="m-0.5 border-2 rounded border-gray-400 hover:border-red-600 hidden lg:inline xl:inline 2xl:inline">
                 0-9
             </li>
         </ul>
