6a733ecb6ee0e3c33d42cddd1a5ae52adf6ca8fc
Matthew Bunting <mtbu@tutanota.com>
2024-09-13 20:48:31 +0100

    Slight style tweaks to search box

--- resources/js/Components/Layouts/Navigation.vue
+++ resources/js/Components/Layouts/Navigation.vue
@@ -5,8 +5,8 @@
                 Clayton&nbsp;TV
             </h1>
             <form class="mr-4 my-2 rounded-2xl relative">
-                <input type="search" id="navbar-search" class="p-2 rounded-xl border-2 border-red-600 text-gray-100 bg-gray-800 focus:outline focus:outline-red-600 focus:bg-gray-700" placeholder="Search" required />
-                <button type="submit" class="absolute end-1 bottom-1 p-1.5 rounded-lg bg-red-600 text-gray-900 hover:bg-red-600 focus:outline focus:outline-1 focus:outline-red-600">Search</button>
+                <input type="search" id="navbar-search" class="p-2 rounded-xl border-2 border-red-600 text-gray-100 bg-gray-800 hover:outline hover:outline-red-600 focus:outline focus:outline-red-600 focus:bg-gray-700" placeholder="Search" required />
+                <button type="submit" class="absolute end-1 bottom-1 p-1.5 rounded-tr-lg rounded-br-lg bg-red-600 text-gray-900 hover:bg-red-600 focus:outline focus:outline-1 focus:outline-red-600">Search</button>
             </form>
         </nav>
     </header>
