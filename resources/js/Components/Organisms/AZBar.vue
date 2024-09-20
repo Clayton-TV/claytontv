48d3d754c5bd781a3b7a1f2ddbec7c7cad3db325
Matthew Bunting <mtbu@tutanota.com>
2024-09-20 21:44:47 +0100

    Split A-Z bar in 3 parts on small screens
    The A-Z bar now splits into 3 parts instead of 2 to fit better

--- resources/js/Components/Organisms/AZBar.vue
+++ resources/js/Components/Organisms/AZBar.vue
@@ -1,22 +1,28 @@
 <script setup>
-    const lettersAM = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'];
-    const lettersNZ = ['N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0-9'];
-    var letters = "A-M";
+    const letters = [
+        ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
+        ['J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'],
+        ['S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0-9']
+    ];
+    let lettersSet = 0;
 
-    function switchLetters() {
-        if (letters == "A-M") {
+    function updateShownLetters(dir) {
+        // dir = 1 for next set, -1 for previous, or leave to refresh display of current set
+        if (dir == 1 && lettersSet < 2) {
             // Hide 1st half of alphabet, show 2nd, move dots to start
-            letters = "N-Z";
-            document.getElementById("lettersDivAM").style.display = "none";
-            document.getElementById("lettersDivNZ").style.display = "contents";
-            document.getElementById("lettersDots").style.order = "-9999";
-        } else {
+            lettersSet++;
+        } else if (dir == -1 && lettersSet > 0) {
             // Hide 2nd half of alphabet, show 1st, move dots to end
-            letters = "A-M";
-            document.getElementById("lettersDivNZ").style.display = "none";
-            document.getElementById("lettersDivAM").style.display = "contents";
-            document.getElementById("lettersDots").style.order = "9999";
+            lettersSet--;
         }
+        for (let i = 0; i < 3; i++) {
+            // Show only the current set of letters (set its div display property to "contents", others to "none")
+            document.getElementById("lettersDiv" + i).style.display = (i == lettersSet) ? "contents" : "none";
+        }
+        // Hide move-forward-dots if on last set of letters
+        document.getElementById("lettersDotsFwd").style.display = (lettersSet == 2) ? "none" : "contents";
+        // Hide move-backward-dots if on first set of letters
+        document.getElementById("lettersDotsBack").style.display = (lettersSet == 0) ? "none" : "contents";
     }
 </script>
 
@@ -28,29 +34,31 @@
         <ul class="grid grid-flow-col auto-cols-fr text-nowrap text-center text-lg" role="radio">
             <!-- Full list will be visible on larger screens -->
             <div id="fullList" class="hidden lg:contents xl:contents 2xl:contents">
-                <li v-for="x in lettersAM" class="m-0.5 border-2 rounded border-gray-400 hover:border-red-600">
-                    {{x}}
-                </li>
-                <li v-for="x in lettersNZ" class="m-0.5 border-2 rounded border-gray-400 hover:border-red-600">
-                    {{x}}
-                </li>
+                <div v-for="lettersGroup in letters" class="contents">
+                    <li v-for="x in lettersGroup" class="m-0.5 border-2 rounded border-gray-400 hover:border-red-600">
+                        {{ x }}
+                    </li>
+                </div>
             </div>
 
-            <!-- For smaller screens the list is split and shown one part at a time -->
+            <!-- For smaller screens the list is split and shown one part at a time, with dots to move fwd/back -->
             <div id="splitList" class="contents lg:hidden xl:hidden 2xl:hidden">
-                <div id="lettersDivAM" class="contents">
-                    <li v-for="x in lettersAM" class="m-0.5 border-2 rounded border-gray-400 hover:border-red-600">
-                        {{x}}
+                <div id="lettersDotsBack" style="display: none">
+                    <li v-on:click="updateShownLetters(-1)">
+                        ...
                     </li>
                 </div>
-                <div id="lettersDivNZ" class="hidden">
-                    <li v-for="x in lettersNZ" class="m-0.5 border-2 rounded border-gray-400 hover:border-red-600">
-                        {{x}}
+                <!-- Create a div per letter group, each with its own id, and only show one to start with -->
+                <div v-for="(lettersGroup, lettersIndex) in letters" v-bind:id="'lettersDiv' + lettersIndex" v-bind:style="lettersSet == lettersIndex ? 'display: contents' : 'display: none'">
+                    <li v-for="x in lettersGroup" class="m-0.5 border-2 rounded border-gray-400 hover:border-red-600">
+                        {{ x }}
                     </li>
                 </div>
-                <li id="lettersDots" v-on:click="switchLetters()">
-                    ...
-                </li>
+                <div id="lettersDotsFwd">
+                    <li v-on:click="updateShownLetters(1)">
+                        ...
+                    </li>
+                </div>
             </div>
         </ul>
     </div>
