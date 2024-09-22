7fad04df63d6692f1c4f5d857a5d9c9c7854127b
Matthew Bunting <mtbu@tutanota.com>
2024-09-22 16:26:58 +0100

    Improve search box visuals

--- resources/js/Components/Layouts/Navigation.vue
+++ resources/js/Components/Layouts/Navigation.vue
@@ -1,22 +1,25 @@
 <template>
     <header class="bg-claytonBlack text-claytonRed">
         <nav
-            class="mx-auto flex flex-wrap items-center justify-between p-6 lg:px-8"
+            class="flex flex-wrap items-center justify-between p-6 lg:px-8"
             aria-label="Global">
-            <h1 class="my-4 ml-4 text-5xl font-bold">Clayton&nbsp;TV</h1>
-            <form class="relative my-2 mr-4 rounded-2xl">
+            <div class="flex items-center"><h1 class="my-4 text-5xl font-bold">Clayton&nbsp;TV</h1></div>
+
+            <!-- Search box -->
+            <form class="my-2 block rounded-xl border-claytonRed hover:outline-claytonRed focus-within:outline-claytonRed bg-gray-800 hover:outline focus-within:outline focus-within:bg-gray-700">
                 <input
                     type="search"
                     id="navbar-search"
-                    class="border-claytonRed hover:outline-claytonRed focus:outline-claytonRed rounded-xl border-2 bg-gray-800 p-2 text-gray-100 hover:outline focus:bg-gray-700 focus:outline"
+                    class="p-2 w-auto text-gray-100 bg-transparent outline-none"
                     placeholder="Search"
                     required />
                 <button
                     type="submit"
-                    class="bg-claytonRed hover:bg-claytonRed focus:outline-claytonRed absolute bottom-1 end-1 rounded-br-lg rounded-tr-lg p-1.5 text-gray-900 focus:outline focus:outline-1">
+                    class="bg-claytonRed hover:bg-claytonRed focus:outline-claytonRed rounded-br-lg rounded-tr-lg p-2 text-gray-900 focus:outline focus:outline-1">
                     Search
                 </button>
             </form>
+
         </nav>
     </header>
 </template>
