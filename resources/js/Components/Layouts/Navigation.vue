ab602a7bc8c1f10b05a9d23a5b802fd0b84de2bc
Matthew Bunting <mtbu@tutanota.com>
2024-10-11 22:27:56 +0100

    Improve layout of buttons on smaller screens

--- resources/js/Components/Layouts/Navigation.vue
+++ resources/js/Components/Layouts/Navigation.vue
@@ -15,25 +15,25 @@
 <template>
     <header class="bg-claytonBlack text-claytonRed">
         <nav
-            class="flex flex-wrap items-center justify-between px-4 py-4 lg:px-6"
+            class="flex flex-wrap items-center justify-between px-4 py-4 lg:px-6 select-none"
             aria-label="Global">
             <h1 class="my-4 text-4xl md:text-5xl lg:text-6xl font-bold">Clayton&nbsp;TV</h1>
 
             <!-- Use so-called "checkbox hack" for the menu. Label is used as menu button and is hidden on larger screens, checkbox itself is always hidden -->
             <input type="checkbox" id="browseMenuCheckbox" hidden></input>
-            <label id="browseMenuCheckboxLabel" for="browseMenuCheckbox" onclick="document.getElementById('moreMenuCheckbox').checked=false" class="border border-claytonRed rounded p-2 lg:hidden">Browse</label>
+            <label id="browseMenuCheckboxLabel" for="browseMenuCheckbox" onclick="document.getElementById('moreMenuCheckbox').checked=false" class="border border-claytonRed rounded p-2 mx-1 ml-auto lg:hidden">Browse</label>
 
             <input type="checkbox" id="moreMenuCheckbox" hidden></input>
-            <label id="moreMenuCheckboxLabel" for="moreMenuCheckbox" onclick="document.getElementById('browseMenuCheckbox').checked=false" class="order-9 border border-claytonRed rounded p-2">More</label>
+            <label id="moreMenuCheckboxLabel" for="moreMenuCheckbox" onclick="document.getElementById('browseMenuCheckbox').checked=false" class="order-9 border border-claytonRed rounded p-2 mx-1">Menu</label>
 
             <div id="browseMenuDiv" class="hidden lg:contents">
                 <div class="block w-full items-center lg:w-auto lg:flex lg:grow lg:place-content-end lg:px-2 order-last lg:order-none">
-                    <a href="#" class="underline font-bold m-3 flex">Categories</a>
-                    <a href="#" class="underline font-bold m-3 flex">Browse All A-Z</a>
-                    <a href="#" class="underline font-bold m-3 flex">Latest Videos</a>
-                    <a href="#" class="underline font-bold m-3 flex">Livestreams</a>
+                    <a href="#" class="underline font-bold mx-2 mb-3 lg:my-0 flex">Categories</a>
+                    <a href="#" class="underline font-bold mx-2 mb-3 lg:my-0 flex">Browse All A-Z</a>
+                    <a href="#" class="underline font-bold mx-2 mb-3 lg:my-0 flex">Latest Videos</a>
+                    <a href="#" class="underline font-bold mx-2 mb-3 lg:my-0 flex">Livestreams</a>
 
-                    <div id="searchBox" class="m-3 lg:mr-0 lg:flex grow min-w-40 lg:max-w-96">
+                    <div id="searchBox" class="mx-2 mb-1 mt-5 lg:my-0 lg:mr-0 lg:flex grow min-w-40 lg:max-w-96">
                         <form class="rounded-xl hover:outline-claytonRed focus-within:outline-claytonRed bg-gray-800 hover:outline focus-within:outline focus-within:bg-gray-700 flex flex-wrap shrink grow">
                             <input
                                 type="search"
@@ -53,10 +53,10 @@
 
             <div id="moreMenuDiv" class="hidden lg:relative w-full order-10">
                 <div class="w-full items-center lg:w-60 lg:absolute bg-claytonBlack border-claytonRed lg:border-2 rounded-tl-xl rounded-b-xl right-0 z-50">
-                    <a href="#" class="underline font-bold m-3 flex" onclick="document.getElementById('moreMenuCheckbox').checked = false;">About</a>
-                    <a href="#" class="underline font-bold m-3 flex" onclick="document.getElementById('moreMenuCheckbox').checked = false;">Contact</a>
-                    <a href="#" class="underline font-bold m-3 flex" onclick="document.getElementById('moreMenuCheckbox').checked = false;">Subscribe</a>
-                    <a href="#" class="underline font-bold m-3 flex" onclick="document.getElementById('moreMenuCheckbox').checked = false;">Another item</a>
+                    <a href="#" class="underline font-bold mx-2 mb-3 flex" onclick="document.getElementById('moreMenuCheckbox').checked = false;">About</a>
+                    <a href="#" class="underline font-bold mx-2 mb-3 flex" onclick="document.getElementById('moreMenuCheckbox').checked = false;">Contact</a>
+                    <a href="#" class="underline font-bold mx-2 mb-3 flex" onclick="document.getElementById('moreMenuCheckbox').checked = false;">Subscribe</a>
+                    <a href="#" class="underline font-bold mx-2 mb-3 flex" onclick="document.getElementById('moreMenuCheckbox').checked = false;">Another item</a>
                 </div>
             </div>
         </nav>
