c7d09d57284f66693565c2f943c9028b8e6f667b
Jamie Gardner <jamie@tgo.dev>
2024-10-20 19:21:47 +0100

    Small tweaks to live

--- resources/js/Components/Pages/Welcome.vue
+++ resources/js/Components/Pages/Welcome.vue
@@ -16,7 +16,7 @@ const livestreams = computed(() => {
 <template>
     <section class="my-10 flex flex-col items-center gap-y-6">
         <div class="space-y-2">
-            <h2 class="text-center text-3xl font-bold">Watch Live</h2>
+            <h2 class="text-center text-3xl text-gray-100 font-bold">Watch Live</h2>
             <p class="text-center text-sm text-gray-400">
                 We're live! Check out the current <br />
                 live streams below.
@@ -30,13 +30,13 @@ const livestreams = computed(() => {
                     :key="video.id"
                     class="shrink-0 snap-center overflow-clip rounded-md bg-gradient-to-br from-gray-700 to-gray-900 mb-3">
                     <div
-                        class="flex gap-y-2 aspect-[10/16] w-auto max-w-[90vw] max-h-[78dvh] flex-col justify-end px-3.5 py-5">
-                        <p class="flex gap-x-1 items-center text-xs font-bold tracking-wide uppercase bg-claytonRed rounded w-fit px-2 py-1">
+                        class="flex gap-y-2 aspect-[10/16] md:aspect-video w-auto max-w-[90vw] max-h-[78dvh] flex-col justify-end px-3.5 py-5">
+                        <p class="flex gap-x-1.5 items-center text-xs font-bold tracking-wide uppercase bg-claytonRed rounded w-fit px-2 py-1">
                              <span class="relative">
-                                 <IconCircleFilled class="absolute opacity-25 animate-ping h-3 w-3" />
+                                 <IconCircleFilled class="absolute opacity-50 animate-ping h-3 w-3" />
                                  <IconCircleFilled class="h-3 w-3" />
                              </span>
-                            <span class="pb-px">Live</span>
+                            <span class="pb-[1.5px]">Live</span>
                         </p>
                         <h3
                             class="line-clamp-2 text-2xl font-bold text-gray-100">
