848f77dfdef2c030f7413a0e62259f52bc6ccb1b
Matthew Bunting <mtbu@tutanota.com>
2024-10-27 17:01:13 +0000

    Move video card list from Welcome to own organism
    This replaces the old contents of VideoCardList.vue

--- resources/js/Components/Pages/Welcome.vue
+++ resources/js/Components/Pages/Welcome.vue
@@ -1,13 +1,14 @@
 <script setup>
 import { computed } from "vue"
-import { IconCircleFilled, IconPlayerPlay } from "@tabler/icons-vue"
+import { IconPlayerPlay } from "@tabler/icons-vue"
 import { Link } from "@inertiajs/vue3"
+import VideoCardList from "@/Organisms/VideoCardList.vue"
 
 const props = defineProps({
     livestreams: {
         type: Array,
     },
-    latestVideos: {
+    latest_videos: {
         type: Array,
         required: true,
     },
@@ -55,57 +56,5 @@ const playVideo = (id) => {
 </script>
 
 <template>
-    <section class="my-10 flex flex-col items-center gap-y-6">
-        <div class="space-y-2">
-            <h2 class="text-center text-3xl font-bold text-gray-100">
-                Watch Live
-            </h2>
-            <p class="text-center text-gray-400">
-                We're live! Check out the current <br />
-                live streams below.
-            </p>
-        </div>
-
-        <div class="mt-2 w-full overflow-x-hidden">
-            <ul class="flex snap-x snap-mandatory gap-x-4 overflow-x-auto px-2">
-                <li
-                    v-for="video in livestreams"
-                    :key="video.id"
-                    class="relative isolate mb-3 flex aspect-[10/16] max-h-[42dvh] w-auto max-w-[90vw] shrink-0 snap-center flex-col justify-end gap-y-2 rounded-md bg-gradient-to-br from-gray-700 to-gray-900 md:aspect-video hover:opacity-75">
-                    <Link :href="`/video/`+video.id"
-                        :id="video.id"
-                        class="absolute inset-0 h-full w-full rounded-md object-cover">
-                        <div class="absolute inset-0 place-self-center">
-                            <div
-                                class="group flex h-min w-min cursor-pointer items-center justify-center rounded-full p-2 hover:bg-gray-100/20">
-                                <IconPlayerPlay
-                                    class="h-14 w-14 stroke-1 text-gray-100"
-                                    @click="playVideo(video.id)" />
-                            </div>
-                        </div>
-                    </Link>
-
-
-                    <div class="flex flex-col px-2.5 py-3.5 sm:px-3.5 sm:py-5 gap-y-1">
-                        <p
-                            class="flex w-fit items-center gap-x-1.5 rounded bg-claytonRed/90 px-1.5 py-0.5 sm:px-2 sm:py-1 text-xs font-bold uppercase tracking-wide">
-                            <span class="relative flex h-2.5 w-2.5">
-                                <span
-                                    class="absolute h-full w-full animate-ping inline-flex opacity-50 bg-white rounded-full" />
-                                <span class="h-full w-full inline-flex relative bg-white rounded-full" />
-                            </span>
-                            <span class="pb-[1.5px]">Live</span>
-                        </p>
-                        <h3
-                            class="line-clamp-1 text-lg leading-snug sm:text-2xl font-bold text-gray-100">
-                            {{ video.name }}
-                        </h3>
-                        <p class="line-clamp-1 truncate text-sm text-gray-400">
-                            10:30 AM Sunday 20th Oct
-                        </p>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </section>
+    <VideoCardList :videos="livestreams" is_livestreams />
 </template>
