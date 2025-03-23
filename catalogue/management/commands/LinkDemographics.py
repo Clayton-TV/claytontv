41911f75f90ee2dc36f586b456a2137f5cb2ebad
Missive00 <jonathan.jf.land@gmail.com>
2025-03-23 17:55:11 +0000

    some comment

--- catalogue/management/commands/LinkDemographics.py
+++ catalogue/management/commands/LinkDemographics.py
@@ -21,6 +21,7 @@ class Command(BaseCommand):
         self.link_demographics("CSV/Demographics.csv",options["DEBUG"]) #calls the function that imports the CSV at the file path into the Bible_Book data table. It also passes the result of options["DEBUG"] which checks if the debug command has been called.
 
     def CleanID(self, ItemIn):
+        # replace with .strip
         ItemOut = ItemIn.replace(" ","")
         ItemOut = ItemOut.replace("'", "")
         ItemOut = ItemOut.replace("[", "")
