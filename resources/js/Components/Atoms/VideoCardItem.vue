29aa3282bb77ca91f895b911d6549c54b5ae8846
Matthew Bunting <mtbu@tutanota.com>
2025-03-29 22:56:24 +0000

    Implement VideoCardGrid for Browse page

--- resources/js/Components/Atoms/VideoCardItem.vue
+++ resources/js/Components/Atoms/VideoCardItem.vue
@@ -23,33 +23,28 @@ const getVideoThumbnail = (videoUrl) => {
 </script>
 
 <template>
-    <div class="absolute inset-0 place-self-center">
-        <div
-            class="group flex h-min w-min cursor-pointer items-center justify-center rounded-full p-2 hover:bg-gray-100/20">
-            <IconPlayerPlay
-                class="h-14 w-14 stroke-1 text-gray-100"/>
+    <div class="w-full h-full flex rounded-md bg-gradient-to-br from-gray-700 to-gray-900">
+        <div class="absolute inset-0 place-self-center group flex h-min w-min cursor-pointer items-center justify-center rounded-full p-2 hover:bg-gray-100/20">
+            <IconPlayerPlay class="h-14 w-14 stroke-1 text-gray-100"/>
+        </div>
+
+        <div class="flex flex-col px-2.5 py-3.5 sm:px-3.5 sm:py-5 gap-y-1 place-self-end">
+            <p class="flex items-center gap-x-1.5 rounded bg-claytonRed/90 px-1.5 py-0.5 sm:px-2 sm:py-1 text-xs font-bold uppercase tracking-wide" v-if="video.is_livestream">
+                <span class="relative flex h-2.5 w-2.5">
+                    <span class="absolute h-full w-full animate-ping inline-flex opacity-50 bg-white rounded-full" />
+                    <span class="h-full w-full inline-flex relative bg-white rounded-full" />
+                </span>
+                <span class="pb-[1.5px]">Live</span>
+            </p>
+            <h3 class="line-clamp-1 text-lg leading-snug sm:text-2xl font-bold text-gray-100">
+                {{ video.name }}
+            </h3>
+            <p class="line-clamp-1 truncate text-sm text-gray-400">
+                10:30 AM Sunday 20th Oct
+            </p>
         </div>
     </div>
 
-
-    <div class="flex flex-col px-2.5 py-3.5 sm:px-3.5 sm:py-5 gap-y-1">
-        <p
-            class="flex w-fit items-center gap-x-1.5 rounded bg-claytonRed/90 px-1.5 py-0.5 sm:px-2 sm:py-1 text-xs font-bold uppercase tracking-wide" v-if="video.is_livestream">
-            <span class="relative flex h-2.5 w-2.5">
-                <span
-                    class="absolute h-full w-full animate-ping inline-flex opacity-50 bg-white rounded-full" />
-                <span class="h-full w-full inline-flex relative bg-white rounded-full" />
-            </span>
-            <span class="pb-[1.5px]">Live</span>
-        </p>
-        <h3
-            class="line-clamp-1 text-lg leading-snug sm:text-2xl font-bold text-gray-100">
-            {{ video.name }}
-        </h3>
-        <p class="line-clamp-1 truncate text-sm text-gray-400">
-            10:30 AM Sunday 20th Oct
-        </p>
-    </div>
 </template>
 
 <style scoped></style>
