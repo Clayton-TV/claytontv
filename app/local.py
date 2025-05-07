d18d0991872851ea1d36cbc7a75e1de36b2409f3
Jamie Gardner <jamie@tgo.dev>
2025-05-07 17:42:52 +0100

    Configure settings from .env file for local and prod

---
+++ app/local.py
@@ -0,0 +1,22 @@
+import os
+
+from .base import *  # noqa: F403
+from .base import BASE_DIR
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-u166q)-sn5ghr5qc7qvkp#1b6594lx!#y$y=%z1$iipepp0@gs")
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+# Database
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    },
+}
---
+++ app/production.py
@@ -0,0 +1,22 @@
+import os
+
+from .base import *  # noqa: F403
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["claytontv.co.uk", "*.claytontv.co.uk"]
+
+# Database
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        # TODO: pull db info from .env
+        # ...
+    },
+}
+
+# Logging
+# TODO
