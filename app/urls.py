d8d4b5002e74c0eb25e6ffcde8297423a3867304
Jamie Gardner <jamie@tgo.dev>
2025-05-11 15:44:45 +0100

    Fix initial route

--- app/urls.py
+++ app/urls.py
@@ -18,8 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
 urlpatterns = [
-    path("", include("app.urls")),
+    path("", views.index, name="index"),
     path("catalogue/", include("catalogue.urls")),
     path("admin/", admin.site.urls),
 ]
