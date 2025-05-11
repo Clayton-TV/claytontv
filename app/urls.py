2b60f735810aaa16bf2bab8ad27114c0cb9884d2
Jamie Gardner <jamie@tgo.dev>
2025-05-11 15:41:58 +0100

    Add an initial route

--- app/urls.py
+++ app/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("app.urls")),
     path("catalogue/", include("catalogue.urls")),
     path("admin/", admin.site.urls),
 ]
