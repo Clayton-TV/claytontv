15b4e5487e21dad15fcc5d24093701c89654561a
Jamie Gardner <jamie@tgo.dev>
2025-05-12 11:41:51 +0100

    Use inertia to render titles

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,6 +1,9 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import {Head} from '@inertiajs/vue3';
+</script>
 
 <template>
+    <Head title="Welcome" />
     <h1>Hello, world!</h1>
     <p>Welcome, Vue.js 3 + TypeScript + Vite + InertiaJS + Django.</p>
 </template>
