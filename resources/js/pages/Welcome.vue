aa6ccbe3dc9ffe001fe28d900db02485676a112d
Jamie Gardner <jamie@tgo.dev>
2025-05-12 12:00:08 +0100

    Coming soon.

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,11 +1,16 @@
 <script lang="ts" setup>
-import {Head} from '@inertiajs/vue3';
+import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
     <Head title="Welcome" />
-    <h1>Hello, world!</h1>
-    <p>Welcome, Vue.js 3 + TypeScript + Vite + InertiaJS + Django.</p>
+    <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div class="text-center">
+            <p class="text-base font-semibold text-blue-600">Coming soon...</p>
+            <h1 class="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">Welcome to Clayton TV</h1>
+            <p class="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">Your source of live and recorded Christian content.</p>
+        </div>
+    </main>
 </template>
 
 <style scoped></style>
