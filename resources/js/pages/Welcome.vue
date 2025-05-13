326ca50484ce3d701acfc38231585934363a207e
Jamie Gardner <jamie@tgo.dev>
2025-05-13 17:21:32 +0100

    Tailwind styles

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -6,9 +6,11 @@ import { Head } from '@inertiajs/vue3';
     <Head title="Welcome" />
     <main class="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
         <div class="text-center">
-            <p class="text-base font-semibold text-blue-600">Coming soon...</p>
-            <h1 class="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">Welcome to Clayton TV</h1>
-            <p class="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">Your source of live and recorded Christian content.</p>
+            <p class="to-accent-foreground bg-gradient-to-tr from-blue-600 from-15% to-75% bg-clip-text text-base font-semibold text-transparent">
+                Coming soon
+            </p>
+            <h1 class="text-secondary-foreground mt-4 text-5xl font-semibold tracking-tight text-balance sm:text-7xl">Welcome to Clayton TV</h1>
+            <p class="text-muted-foreground mt-6 text-lg font-medium text-pretty sm:text-xl/8">Your source of live and recorded Christian content.</p>
         </div>
     </main>
 </template>
