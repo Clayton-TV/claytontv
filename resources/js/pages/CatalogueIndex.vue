2e9c8bbbf12095c06e18ce8f4dce67aa2215a4a0
Jamie Gardner <jamie@tgo.dev>
2025-05-13 18:43:28 +0100

    Install Shadcn-vue UI components

---
+++ resources/js/pages/CatalogueIndex.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import PlaceholderPattern from '@/components/PlaceholderPattern.vue';
+import { BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+import AppLayout from '@/layouts/AppLayout.vue';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Catalogue',
+        href: '/catalogue',
+    },
+];
+</script>
+
+<template>
+    <Head title="Catalogue" />
+    <AppLayout :breadcrumbs>
+        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div class="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
+                    <PlaceholderPattern />
+                </div>
+                <div class="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
+                    <PlaceholderPattern />
+                </div>
+                <div class="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
+                    <PlaceholderPattern />
+                </div>
+            </div>
+            <div class="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 rounded-xl border md:min-h-min">
+                <PlaceholderPattern />
+            </div>
+        </div>
+    </AppLayout>
+</template>
+
+<style scoped></style>
