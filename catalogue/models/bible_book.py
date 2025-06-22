88e35cf73bd0be355420e980a9e36bdc8e879b4d
Jamie Gardner <jamie@tgo.dev>
2025-06-22 14:13:11 +0100

    Skip issue with model underscore

--- catalogue/models/bible_book.py
+++ catalogue/models/bible_book.py
@@ -83,7 +83,7 @@ BIBLE_BOOK_TYPES = (
 )
 
 
-class Bible_Book(models.Model):
+class Bible_Book(models.Model): # noqa N801
     """Model representing a Bible book"""
 
     order = models.CharField(max_length=3, help_text="Integer used to sort books into order.")
