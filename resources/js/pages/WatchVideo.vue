c13ec053c6d3cde390bb408971232b56eab2971b
Matthew Bunting <mtbu@tutanota.com>
2025-09-28 17:00:29 +0100

    Add browse-rest-of-x links to video watching page

--- resources/js/pages/WatchVideo.vue
+++ resources/js/pages/WatchVideo.vue
@@ -1,12 +1,17 @@
 <script setup>
 import { Head } from '@inertiajs/vue3';
 import VideoViewer from '@/organisms/VideoViewer.vue';
+import VideoBrowseByLinks from '@/atoms/VideoBrowseByLinks.vue';
 
 defineProps({
     video: {
         type: Object,
         required: true,
     },
+    video_metadata: {
+        type: Object,
+        required: false,
+    }
 })
 
 </script>
@@ -16,6 +21,8 @@ defineProps({
     <div class="main-container">
         <main class="isolate">
             <VideoViewer :video="video" />
+            <hr class="mx-4 border-gray-800 mb-2 mt-2 lg:mt-4" />
+            <VideoBrowseByLinks :video="video" :video_metadata />
         </main>
     </div>
 </template>
